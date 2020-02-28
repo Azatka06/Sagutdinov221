75486e3e50e84c587b730435ad5fe06e9ccd9d4b
Azat Sagutdinov <azat.sagutdinov@tele2med.ru>
2020-02-28 17:24:09 +0300

    После первой доработки

--- app/src/main/java/com/example/sagutdinov2_2_1/MainActivity.java
+++ app/src/main/java/com/example/sagutdinov2_2_1/MainActivity.java
@@ -48,12 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
         myNoteSharedPref = getSharedPreferences("MyNote", MODE_PRIVATE);
 
-        mBtnSaveNote.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-            }
-        });
     }
 }
 
